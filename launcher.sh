776ed661e590afedc948c0d2390da93da76f06d4
Tim Coy <timothycoy@gmail.com>
2015-05-02 16:08:46 -0400

    fixed directory missing

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo diskutil umount force apps/kerbal/
-mkdir apps/kerbal
+mkdir -p apps/kerbal
 sudo mount 172.16.100.197:/cloudplay/kerbal apps/kerbal/
 ./apps/kerbal/KSP_osx/KSP.app/Contents/MacOS/KSP
